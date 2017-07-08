4ca811c6c54555cce86b395c44c8b5fdcc5bc51d
lx-love <97000433@qq.com>
2017-07-08 16:34:06 +0800

    无敌

---
+++ application/models/registerModels.php
@@ -0,0 +1,17 @@
+<?php
+class registerModels extends CI_Model{
+	
+	//构造函数--直接执行：
+	function registerModels(){
+		//数据库对象连接数据库：
+		$this->load->database();    
+	}
+	
+
+	function insertdata($name,$passwd){
+        $sql = 'insert into user(name,passwd)values("'.$name.'","'.$passwd.'")';
+        $query = $this->db->query($sql);
+        echo $query;
+    }
+}
+    
