d18db580bef0daab072f1d54e22d2f149b87d8f7
Your Name <you@example.com>
2017-07-10 20:13:58 +0800

    购物车界面MODELS更新

--- application/models/shopcarModels.php
+++ application/models/shopcarModels.php
@@ -1,8 +1,8 @@
 <?php
-class SingerModels extends CI_Model{
+class shopcarModels extends CI_Model{
 	
 	//构造函数--直接执行：
-	function SingerModels(){
+	function shopcarModels(){
 		//数据库对象连接数据库：
 		$this->load->database();    
 	}
@@ -10,7 +10,7 @@ class SingerModels extends CI_Model{
 	//定义一个方法：查询数据
 	function getShopingcardata(){
         // 查询数据库，获得的值用变量$query接收：
-        $query = $this->db->query('SELECT * from shopingcar');
+        $query = $this->db->query('SELECT * from index_shopcar');
         // 将接收的数据以数组形式返回：
         return $query->result_array();
     }
