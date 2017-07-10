857842a34ffb10edac314940567b5b62f7fbc223
Your Name <you@example.com>
2017-07-10 19:25:00 +0800

    购物车models

---
+++ application/models/payforModels.php
@@ -0,0 +1,17 @@
+<?php
+class SingerModels extends CI_Model{
+	
+	//构造函数--直接执行：
+	function SingerModels(){
+		//数据库对象连接数据库：
+		$this->load->database();    
+	}
+	
+	//定义一个方法：查询数据
+	function getShopingcardata(){
+        // 查询数据库，获得的值用变量$query接收：
+        $query = $this->db->query('SELECT * from shopingcar');
+        // 将接收的数据以数组形式返回：
+        return $query->result_array();
+    }
+}
