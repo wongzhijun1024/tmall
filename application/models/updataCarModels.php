2ba063fa479f166a48da7a6b785e92b49af97cb3
Your Name <you@example.com>
2017-07-11 16:04:33 +0800

    购物车界面updataCarModels.phpRS更新

--- application/models/updataCarModels.php
+++ application/models/updataCarModels.php
@@ -4,12 +4,11 @@ class updataCarModels extends CI_Model{
         $this->load->database();
     }
     function getprice($id){
-        $sql = 'SELECT price from index_produce2 where id='.$id;
+        $sql = 'SELECT price from index_shopcar where id='.$id;
         // 查询数据库
         $query = $this->db->query($sql);
         // 以数组形式返回查询结果
-//        return $query->result_array();
-        return $query;
+        return $query->result_array();
     }
 
 }
