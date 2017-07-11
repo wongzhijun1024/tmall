c8254cf1e02a80d27e901485183001f6415abfd6
lx-love <97000433@qq.com>
2017-07-11 16:44:50 +0800

    老宋你的快递

--- application/controllers/GoodsControllers.php
+++ application/controllers/GoodsControllers.php
@@ -1,8 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class goodscontrollers extends CI_Controller {
-    function goodscontrollers()
+class GoodsControllers extends CI_Controller {
+    function GoodsControllers()
     {
         parent::__construct();
         $this->load->helper('url');
