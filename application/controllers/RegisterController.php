f708f291c6d5bde6836c38d1b4b68a0f67bef0db
lx-love <97000433@qq.com>
2017-07-11 16:49:43 +0800

    老宋你的快递

--- application/controllers/RegisterController.php
+++ application/controllers/RegisterController.php
@@ -2,8 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 //注意：控制器中的文件和类名，和构造函数名都是一样的；
 //控制器中只有2个程序，一个是构造函数，用于加载类库，一个是解析函数，用于将数据导到前端页面上;
-class registerController extends CI_Controller {
-	function registerController(){
+class RegisterController extends CI_Controller {
+	function RegisterController(){
 		//继承父类的方法，这里父类是框架定义好的：
 		parent::__construct();
 		//添加名叫helper的类库：
