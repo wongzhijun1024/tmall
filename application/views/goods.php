6876f676665edcab4cf8e8a045a00f66899ca908
Your Name <you@example.com>
2017-07-12 13:37:04 +0800

    搜索界面

--- application/views/goods.php
+++ application/views/goods.php
@@ -10,6 +10,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" href="<?php echo base_url() ?>css/page2css.css" type="text/css">
 </head>
 <body>
-<h1>商品详细</h1>
+<h1>商品搜索</h1>
 </body>
 </html>
